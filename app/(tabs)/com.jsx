8c7590b80561883cbc672a26a020f35037f73e59
abhisek1221 <cherrymx317@gmail.com>
2024-04-29 02:58:45 +0530

    getAllPosts | getLatestPosts fetch UseAppwrite

--- app/(tabs)/com.jsx
+++ app/(tabs)/com.jsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import React from 'react'
+import { getAllPosts } from '../../lib/appwrite';
+import { useEffect, useState } from 'react';
+import useAppwrite from '../../lib/useAppwrite';
+
 
 const com = () => {
-  return (
-    <View>
+  const {data : posts} = useAppwrite(getAllPosts);
+  console.log(posts);
+      return (
+    <View className="h-screen bg-primary">
       <Text>Community</Text>
     </View>
   )
