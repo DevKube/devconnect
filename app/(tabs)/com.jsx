4059e974d6704fc29791f3de6b3f254878a14990
abhisek1221 <cherrymx317@gmail.com>
2024-04-29 03:24:34 +0530

    mediaCard + animatable + fetchFlatlist

--- app/(tabs)/com.jsx
+++ app/(tabs)/com.jsx
@@ -1,17 +1,30 @@
-import { StyleSheet, Text, View, Alert } from 'react-native'
+import { StyleSheet, Text, View, Alert, FlatList, ScrollView } from 'react-native'
 import React from 'react'
 import { getAllPosts } from '../../lib/appwrite';
 import { useEffect, useState } from 'react';
 import useAppwrite from '../../lib/useAppwrite';
+import MediaCard from '../../components/MediaCard';
 
 
 const com = () => {
   const {data : posts} = useAppwrite(getAllPosts);
-  console.log(posts);
       return (
-    <View className="h-screen bg-primary">
-      <Text>Community</Text>
-    </View>
+        <ScrollView className="h-screen bg-primary">
+     <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <MediaCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            media={item.media}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+      />
+        </ScrollView>
+    
   )
 }
 
