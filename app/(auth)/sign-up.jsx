62c7cbec1456bbb0e4d312f9fff47b7626560d10
abhisek1221 <cherrymx317@gmail.com>
2024-04-28 14:50:04 +0530

    SignUp-Page

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,93 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-const SignUp = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
-  )
-}
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image, StyleSheet } from "react-native";
+import CustomButton from "../../components/CustomButton";
+import FormField from "../../components/FormField";
+import LottieView from "lottie-react-native";
+import { images } from "../../constants";
 
-export default SignUp
+const SignUp = () => {
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
+          <View className="flex-row justify-center items-center">
+          <LottieView source={require('../../assets/animations/sphere.json')}
+        autoPlay loop className="w-[100px] h-[64px]"resizeMode="contain"
+        />
+        <Text className="font-pbold text-purple-500 text-3xl">DevConnect</Text>
+          </View>
+       
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Sign Up to DevConnect
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-purple-500"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
